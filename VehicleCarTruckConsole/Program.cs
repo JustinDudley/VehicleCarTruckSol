86c73fbd483ef8c52d0bf225a124725c647d460c
Justin Dudley <justindudley99@gmail.com>
2019-09-04 09:18:00 -0400

    Add project files.

---
+++ VehicleCarTruckConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VehicleCarTruckConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
